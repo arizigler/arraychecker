225ac05f60181f0156b9c5f1fae96bf09faeee25
Hagai <hagaiattias@gmail.com>
2013-03-08 02:03:56 +0200

    A class to test the analysis on.

---
+++ SwAnalysisProj/src/test/TestClass.java
@@ -0,0 +1,16 @@
+package test;
+public class TestClass {
+
+	public static void main(String[] args) {
+		String array[] = args;
+		int a = 1 + 2;
+		int b = 2;
+		int c = 3;
+		int d = a + 5;
+		Object h = new Object();
+		boolean bool = args.length == 0;
+		if (bool) {
+			int e = 3;
+		}
+	}
+}
