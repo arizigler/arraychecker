524aec21eb9a9b207f9257391ef725b5e1c37ad7
Hagai <hagaiattias@gmail.com>
2013-03-11 01:14:28 +0200

    Added MultTest

--- SwAnalysisProj/src/tests/TestArrayDef.java
+++ SwAnalysisProj/src/tests/TestArrayDef.java
@@ -7,17 +7,22 @@ public class TestArrayDef {
 
 		int array5[] = new int[5]; // array5 => array of size 5
 
-		int array2[] = new int[2];
+		int array2[] = new int[-10];
 
 		int i = 0;
+		int t = 20;
 		int g = 2;
 		int x = -5;
+
+		int p = x * g; // p is [-10,-10]
+
+		array5[p] = 5; // Illegal lower bound
 
 		array5[10] = 5; // Illegal upper bound
 
 		array5[1] = 4; // Good
 
-		array5[i] = 6; // Illegal upper bound
+		array5[t] = 6; // Illegal upper bound
 
 		array5[g] = 3; // Good
 
---
+++ SwAnalysisProj/src/tests/TestMult.java
@@ -0,0 +1,65 @@
+package tests;
+
+@SuppressWarnings("unused")
+public class TestMult {
+
+	public static void main(String[] args) {
+
+		// arrayX => array of size X
+		int array0[] = new int[0];
+		int array5[] = new int[5];
+		int array16[] = new int[16];
+		int array17[] = new int[17];
+		int array19[] = new int[19];
+		int array200[] = new int[200];
+
+		// iX => integer with value X
+		int i0 = 0;
+		int i20 = 20;
+		int i2 = 2;
+		int i3 = 3;
+		int i4 = 4;
+		int i_5 = -5;
+		int p = i2 * i_5; // p is [-10,-10]
+
+		array0[0] = 5; // Illegal upper bound
+
+		array0[1] = 5; // Illegal upper bound
+
+		array5[p] = 5; // Illegal lower bound
+
+		p = 2 * i2; // p is [4,4]
+
+		array5[p] = 5; // Good
+
+		p *= p; // p is [16,16]
+
+		array16[p] = 5; // Illegal upper bound (legal index range is 0-15)
+
+		array17[p] = 5; // Good
+
+		p = i2 * i3; // p is [6,6]
+
+		boolean b = args.length == 0;
+
+		if (b) {
+			p = p * i3; // p is [18,18]
+		}
+
+		// now p is [6,18]
+
+		array17[p] = 5; // Potentially illegal upper bound
+		array19[p] = 5; // Good
+
+		for (int i = 0; i < 5; i++) {
+			p *= 2;
+		}
+
+		// now p is [-INF,INF]
+
+		array19[p] = 5; // Potentially illegal upper bound
+
+		array200[p] = 5; // Potentially illegal upper bound
+
+	}
+}
